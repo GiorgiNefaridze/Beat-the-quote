8b125068f059a72a39970d48d0f8a70df9bb28a7
Giorgi Nefaridze <nefogio1@gmail.com>
2022-12-12 23:14:13 +0400

    make user context

---
+++ client/src/context/UserContext.tsx
@@ -0,0 +1,40 @@
+import { useState, createContext, useContext, useEffect } from "react";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+interface IUser {
+  username: string;
+  email: string;
+  image: string | null;
+}
+
+interface IUserContext {
+  user: IUser;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+}
+
+const userContext = createContext<IUserContext>({} as IUserContext);
+
+export const UserContext = () => {
+  return useContext(userContext);
+};
+
+export const UserContextProvider = ({ children }: IProps) => {
+  const [user, setUser] = useState({} as IUser);
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("user") || "false");
+
+    if (userInfo) {
+      setUser(userInfo);
+    }
+  }, []);
+
+  return (
+    <userContext.Provider value={{ user, setUser }}>
+      {children}
+    </userContext.Provider>
+  );
+};
