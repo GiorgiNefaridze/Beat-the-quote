c4229e5ca9fbaf09f9167e833aaa3a5c0493709c
Giorgi Nefaridze <nefogio1@gmail.com>
2022-12-16 00:58:12 +0400

    display quote and make differencies between fetched and written quote

--- client/src/components/GamePage/GamePage.tsx
+++ client/src/components/GamePage/GamePage.tsx
@@ -8,17 +8,23 @@ import { DifficultyContext } from "../../context/DifficultyContext";
 import { GamePageWrapper } from "./GamePage.style";
 
 const GamePage: React.FC = () => {
-  const [quote, setQuote] = useState<string>("");
+  const [quote, setQuote] = useState<string[]>([]);
+  const [writtenQuote, setWrittenQuote] = useState<string[]>([]);
 
   const { difficulty } = DifficultyContext();
   const { getQuote, loading } = useGetQuote();
   const navigate = useNavigate();
 
   useEffect(() => {
+    let letterOfQuote = [];
     if (difficulty) {
       (async () => {
         const { text, author } = await getQuote(difficulty);
-        setQuote(text);
+        for (let i = 0; i < text.length; i++) {
+          letterOfQuote.push(text[i]);
+        }
+
+        setQuote(letterOfQuote);
       })();
 
       return;
@@ -26,11 +32,30 @@ const GamePage: React.FC = () => {
     navigate("/");
   }, []);
 
+  onkeydown = (e: any) => {
+    const { key } = e;
+
+    if (quote && key === quote[0]) {
+      setWrittenQuote([...writtenQuote, quote[0]]);
+      setQuote(quote.slice(1));
+
+      if (quote.length === 1) {
+        
+        return;
+      }
+    }
+  };
+
   return (
     <GamePageWrapper>
-      <h1>
-        <q>{quote}</q>
-      </h1>
+      <q style={{ display: "flex" }}>
+        {writtenQuote?.map((letter) => (
+          <h1 style={{ background: "green", whiteSpace: "pre" }}>{letter}</h1>
+        ))}
+        {quote?.map((letter) => (
+          <h1 style={{ whiteSpace: "pre" }}>{letter}</h1>
+        ))}
+      </q>
     </GamePageWrapper>
   );
 };
