200c256de7f2a4c818d16dff1f02ee05222f07ad
Giorgi Nefaridze <nefogio1@gmail.com>
2022-12-15 16:49:26 +0400

    make routes in application

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,23 @@
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { DifficultyContextProvider } from "./context/DifficultyContext";
 import { UserContextProvider } from "./context/UserContext";
 import { AuthContextProvider } from "./context/AuthPopUpContext";
 
+import App from "./App";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <AuthContextProvider>
-    <UserContextProvider>
-      <DifficultyContextProvider>
-        <App />
-      </DifficultyContextProvider>
-    </UserContextProvider>
-  </AuthContextProvider>
+  <Router>
+    <AuthContextProvider>
+      <UserContextProvider>
+        <DifficultyContextProvider>
+          <App />
+        </DifficultyContextProvider>
+      </UserContextProvider>
+    </AuthContextProvider>
+  </Router>
 );
